17e743fc22d9d85c295fc2893dd2f35630d07fd7
Ryan Smith <rnsmith2@gmail.com>
2015-07-21 15:37:14 -0700

    Initial commit
    Include use-package, magit, company, and smex

---
+++ init.el
@@ -0,0 +1,40 @@
+;; Initialize emacs package manager:
+(require 'package)
+;; Setup community package archive
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package', a convenience wrapper to the package manager
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Only need to load use-package when compiling
+(require 'use-package)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Actually start loading desired packages and configuration                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; company-mode is a modern auto-completion tool
+;; http://company-mode.github.io/
+(use-package company
+  :ensure t
+  :config (progn (setq company-idle-delay .2)
+                 (bind-key "C-n" 'company-select-next company-active-map)
+                 (bind-key "C-p" 'company-select-previous company-active-map)
+                 (global-company-mode)))
+
+;; fuzzy autocompletion of emacs commands
+;; https://github.com/nonsequitur/smex
+(use-package smex
+  :ensure t
+  :config (smex-initialize)
+  :bind (("M-x" . smex)
+         ("M-X" . smex-major-mode-commands)))
+
+;; git wrapper / advanced in emacs git menu
+;; https://github.com/magit/magit
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
